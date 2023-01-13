1fa3f77006cde672c25c9cf580be892d11c2eb1d
Milk Salintip <milk@192.168.1.102>
2023-01-13 16:28:53 +0700

    success keyword, link, comment file. and success suffle keyword.

--- code_for_backlink.py
+++ code_for_backlink.py
@@ -1,46 +1,63 @@
 import random
 import webbrowser
-<<<<<<< HEAD
 import codecs
 import html
- 
-=======
 
-#TEST
-
->>>>>>> d6e0275385e215ff27337933bb499c80f394e351
 def main():
     
     # Open the file and read all lines
-    with open("file.txt", "r") as file:
+    with open("keyword.txt", "r") as file:
         lines = file.readlines()
-
-    # Select a random line from the file
-    anchor_text1 = random.choice(lines)
-    keyword1 = anchor_text1.strip()
     
-    anchor_text2 = random.choice(lines)
-    keyword2 = anchor_text2.strip()
+    with open("comment.txt", "r") as file:
+        lines_comment = file.readlines()
     
-    anchor_text3 = random.choice(lines)
-    keyword3 = anchor_text3.strip()
+    with open("link.txt", "r") as file:
+        link = file.readlines()
+        
+    # Select a random set of unique lines from the file
+    unique_lines = random.sample(lines, 3)
+    unique_lines_comment = random.sample(lines_comment, 1)
+    
+    # Shuffle the unique lines
+    random.shuffle(unique_lines)
 
     # Use the random line as the anchor text and keyword
-    link = "https://www.google.com"
-    anchor_link1 = f'<a href="{link}" keyword="{keyword1}">{anchor_text1}</a> '
-    anchor_link2 = f'<a href="{link}" keyword="{keyword2}">{anchor_text2}</a> '
-    anchor_link3 = f'<a href="{link}" keyword="{keyword3}">{anchor_text3}</a> '
+    link = link[0]
+    anchor_link1 = f'<a href="{link}" keyword="{unique_lines[0].strip()}" rel="nofollow ugc">{unique_lines[0]}</a> '
+    anchor_link2 = f'<a href="{link}" keyword="{unique_lines[1].strip()}" rel="nofollow ugc">{unique_lines[1]}</a> '
+    anchor_link3 = f'<a href="{link}" keyword="{unique_lines[2].strip()}" rel="nofollow ugc">{unique_lines[2]}</a> '
+    
+    bbcode1 = f'[url={link}]{unique_lines[0]}[/url] '
+    bbcode2 = f'[url={link}]{unique_lines[1]}[/url] '
+    bbcode3 = f'[url={link}]{unique_lines[2]}[/url] '
 
     # Write the anchor link to an HTML file
     with open("output.html", "w") as file:
         file.write("<!DOCTYPE html><html><head><title>Code For Backlink</title></head><body>")
-        file.write(anchor_link1)
-        file.write("<pre><code>")
-        file.write(html.escape( anchor_link2 ))
-        file.write("</code></pre>")
-        file.write(anchor_link3)
-        file.write("</body></html>")
+        
+        file.write( "<h1>1. ลิงค์แบบ Anchor link</h1>" )
+        
+        file.write( f'{unique_lines_comment[0]}' + " ")
+        file.write(anchor_link1 + "<br />")
+        file.write(anchor_link2 + "<br />")
+        file.write(anchor_link3 + "<br />")
+        
+        
+        file.write( "<h1>2. ลิงค์แบบ HTML</h1>" )
+        file.write(f'{unique_lines_comment[0]}' + " ")
+        file.write(html.escape( anchor_link1 ) + "<br />")
+        file.write(html.escape( anchor_link2 ) + "<br />")
+        file.write(html.escape( anchor_link3 ) + "<br />")
+        
+        file.write( "<h1>3. ลิงค์แบบ BB Code</h1>" )
+        file.write(f'{unique_lines_comment[0]}' + " ")
+        file.write(bbcode1 + "<br />")
+        file.write(bbcode2 + "<br />")
+        file.write(bbcode3 + "<br />")
 
+        file.write("</body></html>")
+        
 if __name__ == "__main__":
     main()
     webbrowser.open("output.html")
