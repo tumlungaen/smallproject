e20e5c24498fcbebfd6a0f8400d2d70d62e95482
tumlungaen <mr.kittinan@gmail.com>
2023-01-16 17:27:52 +0700

    try example of article.txt and topic.txt

--- pbn/genPBNblog.py
+++ pbn/genPBNblog.py
@@ -1,11 +1,11 @@
 import webbrowser
 
 # Open the HTML template file
-with open('seo_structure.html', 'r', encoding="UTF-8") as file:
+with open('seo_structure.html', 'r') as file:
     html = file.read()
 
 # Open the keywords file
-with open('setting.txt', 'r', encoding="UTF-8") as file:
+with open('setting.txt', 'r') as file:
     settings = file.readlines()
     settings_dict = {}
     for setting in settings:
